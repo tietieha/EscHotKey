371e209f7480b5b221a28fdde70bfdb5cf07b416
leon <leon@im30.lan>
2024-07-29 17:27:56 +0800

    [fix]限制多个实例运行

--- EscHotKey/AutoCloseWnd.cpp
+++ EscHotKey/AutoCloseWnd.cpp
@@ -1,4 +1,4 @@
-// AutoCloseWnd.cpp : ����Ӧ�ó��������Ϊ��
+// AutoCloseWnd.cpp : 定义应用程序的类行为。
 //
 
 #include "stdafx.h"
@@ -9,7 +9,8 @@
 #define new DEBUG_NEW
 #endif
 
-
+// 生成GUID的互斥体名称
+#define MUTEX_NAME _T("EscHotkey_Mutex")
 // CAutoCloseWndApp
 
 BEGIN_MESSAGE_MAP(CAutoCloseWndApp, CWinApp)
@@ -17,31 +18,42 @@ BEGIN_MESSAGE_MAP(CAutoCloseWndApp, CWinApp)
 END_MESSAGE_MAP()
 
 
-// CAutoCloseWndApp ����
+// CAutoCloseWndApp 构造
 
 CAutoCloseWndApp::CAutoCloseWndApp()
 {
-	// TODO: �ڴ˴����ӹ�����룬
-	// ��������Ҫ�ĳ�ʼ�������� InitInstance ��
+	// TODO: 在此处添加构造代码，
+	// 将所有重要的初始化放置在 InitInstance 中
 }
 
 
-// Ψһ��һ�� CAutoCloseWndApp ����
+// 唯一的一个 CAutoCloseWndApp 对象
 
 CAutoCloseWndApp theApp;
 
 
-// CAutoCloseWndApp ��ʼ��
+// CAutoCloseWndApp 初始化
 
 BOOL CAutoCloseWndApp::InitInstance()
 {
-	// ���һ�������� Windows XP �ϵ�Ӧ�ó����嵥ָ��Ҫ
-	// ʹ�� ComCtl32.dll �汾 6 ����߰汾�����ÿ��ӻ���ʽ��
-	//����Ҫ InitCommonControlsEx()�����򣬽��޷��������ڡ�
+	// 创建一个互斥体
+	HANDLE hMutex = CreateMutex(NULL, TRUE, MUTEX_NAME);
+
+	// 检查错误代码
+	if (GetLastError() == ERROR_ALREADY_EXISTS)
+	{
+		// 如果互斥体已经存在，说明已有一个实例在运行
+		AfxMessageBox(_T("应用程序已经在运行。"));
+		return FALSE; // 阻止新实例启动
+	}
+
+	// 如果一个运行在 Windows XP 上的应用程序清单指定要
+	// 使用 ComCtl32.dll 版本 6 或更高版本来启用可视化方式，
+	//则需要 InitCommonControlsEx()。否则，将无法创建窗口。
 	INITCOMMONCONTROLSEX InitCtrls;
 	InitCtrls.dwSize = sizeof(InitCtrls);
-	// ��������Ϊ��������Ҫ��Ӧ�ó�����ʹ�õ�
-	// �����ؼ��ࡣ
+	// 将它设置为包括所有要在应用程序中使用的
+	// 公共控件类。
 	InitCtrls.dwICC = ICC_WIN95_CLASSES;
 	InitCommonControlsEx(&InitCtrls);
 
@@ -49,30 +61,43 @@ BOOL CAutoCloseWndApp::InitInstance()
 
 	AfxEnableControlContainer();
 
-	// ��׼��ʼ��
-	// ���δʹ����Щ���ܲ�ϣ����С
-	// ���տ�ִ���ļ��Ĵ�С����Ӧ�Ƴ�����
-	// ����Ҫ���ض���ʼ������
-	// �������ڴ洢���õ�ע�����
-	// TODO: Ӧ�ʵ��޸ĸ��ַ�����
-	// �����޸�Ϊ��˾����֯��
-	SetRegistryKey(_T("Ӧ�ó��������ɵı���Ӧ�ó���"));
+	// 标准初始化
+	// 如果未使用这些功能并希望减小
+	// 最终可执行文件的大小，则应移除下列
+	// 不需要的特定初始化例程
+	// 更改用于存储设置的注册表项
+	// TODO: 应适当修改该字符串，
+	// 例如修改为公司或组织名
+	SetRegistryKey(_T("应用程序向导生成的本地应用程序"));
 
 	CAutoCloseWndDlg dlg;
 	m_pMainWnd = &dlg;
 	INT_PTR nResponse = dlg.DoModal();
 	if (nResponse == IDOK)
 	{
-		// TODO: �ڴ˷��ô�����ʱ��
-		//  ��ȷ�������رնԻ���Ĵ���
+		// TODO: 在此放置处理何时用
+		//  “确定”来关闭对话框的代码
 	}
 	else if (nResponse == IDCANCEL)
 	{
-		// TODO: �ڴ˷��ô�����ʱ��
-		//  ��ȡ�������رնԻ���Ĵ���
+		// TODO: 在此放置处理何时用
+		//  “取消”来关闭对话框的代码
 	}
 
-	// ���ڶԻ����ѹرգ����Խ����� FALSE �Ա��˳�Ӧ�ó���
-	//  ����������Ӧ�ó������Ϣ�á�
+	// 由于对话框已关闭，所以将返回 FALSE 以便退出应用程序，
+	//  而不是启动应用程序的消息泵。
 	return FALSE;
+}
+
+
+int CAutoCloseWndApp::ExitInstance()
+{
+	// 在应用程序退出时释放互斥体
+	HANDLE hMutex = OpenMutex(MUTEX_ALL_ACCESS, FALSE, MUTEX_NAME);
+	if (hMutex)
+	{
+		CloseHandle(hMutex);
+	}
+
+	return CWinApp::ExitInstance();
 }
--- EscHotKey/AutoCloseWnd.h
+++ EscHotKey/AutoCloseWnd.h
@@ -1,17 +1,17 @@
-// AutoCloseWnd.h : PROJECT_NAME Ó¦ÓÃ³ÌĞòµÄÖ÷Í·ÎÄ¼ş
+// AutoCloseWnd.h : PROJECT_NAME åº”ç”¨ç¨‹åºçš„ä¸»å¤´æ–‡ä»¶
 //
 
 #pragma once
 
 #ifndef __AFXWIN_H__
-	#error "ÔÚ°üº¬´ËÎÄ¼şÖ®Ç°°üº¬¡°stdafx.h¡±ÒÔÉú³É PCH ÎÄ¼ş"
+	#error "åœ¨åŒ…å«æ­¤æ–‡ä»¶ä¹‹å‰åŒ…å«â€œstdafx.hâ€ä»¥ç”Ÿæˆ PCH æ–‡ä»¶"
 #endif
 
-#include "resource.h"		// Ö÷·ûºÅ
+#include "resource.h"		// ä¸»ç¬¦å·
 
 
 // CAutoCloseWndApp:
-// ÓĞ¹Ø´ËÀàµÄÊµÏÖ£¬Çë²ÎÔÄ AutoCloseWnd.cpp
+// æœ‰å…³æ­¤ç±»çš„å®ç°ï¼Œè¯·å‚é˜… AutoCloseWnd.cpp
 //
 
 class CAutoCloseWndApp : public CWinApp
@@ -19,11 +19,12 @@ class CAutoCloseWndApp : public CWinApp
 public:
 	CAutoCloseWndApp();
 
-// ÖØĞ´
+// é‡å†™
 	public:
 	virtual BOOL InitInstance();
+	virtual int ExitInstance();
 
-// ÊµÏÖ
+// å®ç°
 
 	DECLARE_MESSAGE_MAP()
 };
